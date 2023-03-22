5f09ef26fc7ad06a6235d5f2624865deebb6d49a
badrboubric <61752607+badrboubric@users.noreply.github.com>
2023-03-22 20:31:54 +0100

    added forms

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get Popup Container Element
+const popupContainer = document.querySelector('.popup-container');
+
+// Show Modal after Delay Time (in milliseconds)
+setTimeout(function(){
+  // Display Popup with Gradual Scalling Up Transition Effect
+  popupContainer.style.display = "block";
+}, 2000); // Wait for Two Seconds;
